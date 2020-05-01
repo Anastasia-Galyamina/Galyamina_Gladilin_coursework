e541f873534449628935dcdeef8defe198544e8c
Anastasia-Galyamina <52350714+Anastasia-Galyamina@users.noreply.github.com>
2020-05-01 23:17:44 +0400

    Update CreditBindingModel.cs

--- BankBusinessLogic/BindingModels/CreditBindingModel.cs
+++ BankBusinessLogic/BindingModels/CreditBindingModel.cs
@@ -7,7 +7,7 @@ namespace BankBusinessLogic.BindingModels
     public class CreditBindingModel
     {
         public int? Id { get; set; }
-        public string CreditName { get; set; }
+        public CreditType Type { get; set; }
         public int CountMoney { get; set; }
         public DateTime? DateImplement { get; set; }
         public string currency { get; set; }
